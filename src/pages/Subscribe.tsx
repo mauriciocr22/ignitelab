2113a278275ae863df564231a2e8ecc7a9045bfe
Maurício de Carvalho <mauriciocr223@gmail.com>
2022-06-27 19:42:11 -0300

    fix: showing up image in subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import imgUrl from '../../src/assets/code-mockup.png'
 
 export function Subscribe() {
     const navigate = useNavigate()
@@ -66,7 +67,7 @@ export function Subscribe() {
                 </div>
             </div>
 
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={imgUrl} className="mt-10" alt="" />
         </div>
     )
 }
